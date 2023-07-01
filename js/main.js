071a851490557dfd3d41d24b822000657dd0500e
Swapnil2411 <swapnil.darge@fugetroncorp.com>
2023-07-02 02:38:28 +0530

    Files Modified

--- js/main.js
+++ js/main.js
@@ -122,8 +122,60 @@ $(document).ready(function(){
     //     }
     // })
 
-    // $('.count-up').countUp({
-    //     'time': 2000,
-    // });
+    $('.count-up').countUp({
+        'time': 2000,
+    });
     
 })
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Split text into spans
+    let typeSplit = new SplitType("[lines-split]", {
+      types: "lines, words",
+      tagName: "span"
+    });
+
+  
+    // Link timelines to scroll position
+    function createScrollTrigger(triggerElement, timeline, startPoint) {
+      // Reset tl when scroll out of view past bottom of screen
+      ScrollTrigger.create({
+        trigger: triggerElement,
+        start: startPoint,
+        onLeaveBack: () => {
+          timeline.progress(0);
+          timeline.pause();
+        }
+      });
+      // Play tl when scrolled into view (60% from top of screen) "top 60%"
+      ScrollTrigger.create({
+        trigger: triggerElement,
+        start: startPoint,
+        onEnter: () => timeline.play()
+      });
+    }
+    $("[lines-slide-up]").each(function (index) {
+      let tl = gsap.timeline({ paused: true });
+      tl.from($(this).find(".line"), { opacity: 0, yPercent: 150, duration: 0.8, ease: "ease-in-out", stagger: { amount: 0.5 } });
+      createScrollTrigger($(this), tl, "top 60%");
+    });
+   
+    
+    $("[lines-parent]").each(function (index) {
+      let tl = gsap.timeline({ paused: true });
+      tl.from($(this), { opacity: 0, yPercent: 20, duration: 0.8, ease: "ease-in-out", stagger: { amount: 0.5 } });
+      createScrollTrigger($(this), tl, "top 60%");
+    });
+    
+     
+    $("[sec-scale]").each(function (index) {
+        let tl = gsap.timeline({ paused: true });
+        tl.to($(this), { scale: 1, duration: 1.5, ease: "ease-in-out", markers:true,
+        pin:true,
+        scrub: true});
+        createScrollTrigger($(this), tl, "top 80%");
+    });
+    
+    // Avoid flash of unstyled content
+    gsap.set("[text-split]", { opacity: 1 });
+  });
